b035d515d436851e2638bc442ade84178c1e3d16
jraihan-me <jahirraihanmsd@gmail.com>
2021-07-06 13:42:36 +0600

    book show and routing done

--- books/views.py
+++ books/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import json
+
+booksData = open(
+    '/home/jahir/Downloads/codes/django/bookstore-project/books.json').read()
+
+data = json.loads(booksData)
 
 
 def index(request):
-    context = {'name': 'Jahir'}
+    context = {'books': data}
     return render(request, 'books/index.html', context)
+
+
+def show(request, id):
+    singlebook = list()
+    for book in data:
+        if book['id'] == id:
+            singlebook = book
+    context = {'book': singlebook}
+    return render(request, 'books/show.html', context)
